32dfbbd66378a3c79b0b80f6928e0a44b705d1dd
motivated24 <motivated24@hotmail.com>
2017-10-18 15:03:22 +0800

    深度优先搜索（递归法）

--- src/depthFirstSearch/DFSTest.java
+++ src/depthFirstSearch/DFSTest.java
@@ -6,77 +6,104 @@ package depthFirstSearch;
  * */
 public class DFSTest {
 
-	//存储节点
-	private char[] vertices;
+	//存储节点信息
+	private char[] nodes;
 	
-	//存储边信息（邻接矩阵）
-	private int[][] arcs;
+	//存储边信息（如果节点连接，为1，否则为0）
+	private int[][] edges;
 	
-	//图的节点数
-	private int vexnum;
+	//节点个数
+	private int nodesNum;
 	
-	//记录节点是否被遍历过
+	//边是否被遍历过
 	private boolean[] visited;
 	
-	//初始化
+	/**
+	 * 初始化
+	 * 1. 节点个数
+	 * 2. 节点连接信息
+	 * 3. 节点是否被遍历过
+	 * 
+	 * */
 	public DFSTest(int num) {
 		
-		vexnum = num;
-		vertices = new char[num];
-		arcs = new int[num][num];
+		nodesNum = num;
+		nodes = new char[num];
+		edges = new int[num][num];
 		visited = new boolean[num];
 		
-		for (int i = 0; i < vexnum; i++) {
-			for (int j = 0; j < vexnum; j++) {
+		for (int i = 0; i < nodesNum; i++) {
+			for (int j = 0; j < nodesNum; j++) {
 				
-				arcs[i][j] = 0;
+				edges[i][j] = 0;
 			}
-		}	
+		}
 	}
 	
-	//添加边（无向图）
-	public void addEdge(int i, int j) {
+	/**
+	 * 添加边信息（节点连接则设置为1，否则为0）
+	 * 
+	 * @param num1 通过边连接的节点1
+	 * @param num2 通过边连接的节点2
+	 * 
+	 * */
+	public void addEdge(int num1, int num2) {
 		
-		if (i == j) {
+		if (num1 == num2) {
 			
 			return;
+		} else {
+			
+			edges[num1][num2] = 1;
+			edges[num2][num1] = 1;
 		}
-		
-		arcs[i][j] = 1;
-		arcs[j][i] = 1;
 	}
 	
-	//设置节点集
-	public void setVertices(char[] vertices) {
+	/**
+	 * 设置节点集
+	 * @param nodes 节点
+	 * 
+	 * */
+	public void setNodes(char[] nodes) {
 		
-		this.vertices = vertices;
+		this.nodes = nodes;
 	}
 	
-	//设置节点访问标记
-	public void setVisited(boolean[] visted) {
+	/**
+	 * 设置节点访问标记
+	 * @param visited 访问标记
+	 * 
+	 * */
+	public void setVisited(boolean[] visited) {
 		
-		this.visited = visted;
+		this.visited = visited;
 	}
 	
-	//打印遍历节点
+	/**
+	 * 打印遍历节点
+	 * @param num 节点
+	 * 
+	 * */
 	public void visit(int i) {
 		
-		System.out.print(vertices[i] + " ");
+		System.out.print(nodes[i] + " ");
 	}
 	
-	//从第i个节点开始深度优先遍历
+	/**
+	 * 从第i个节点开始深度优先遍历
+	 * 
+	 * @param i 第i个节点
+	 * 
+	 * */
 	private void travel(int i) {
 		
-		//标记第i个节点已遍历
 		visited[i] = true;
 		
-		//打印当前遍历的节点
 		visit(i);
 		
-		//遍历邻接矩阵中第i个节点的直接连通关系
-		for (int j = 0; j < vexnum; j++) {
+		for (int j = 0; j < nodesNum; j++) {
 			
-			if (arcs[i][j] == 1 && visited[j] == false) {
+			if (1 == edges[i][j] && false == visited[j]) {
 				
 				travel(j);
 			}
@@ -87,17 +114,16 @@ public class DFSTest {
 	public void DFSTravel() {
 		
 		//初始化节点遍历标记
-		for (int i = 0; i < vexnum; i++) {
+		for (int i = 0;  i < nodesNum; i++) {
 			
 			visited[i] = false;
 		}
 		
-		//从没有被遍历的节点开始遍历
-		for (int i = 0; i < vexnum; i++) {
+		//从没有遍历过的节点开始遍历
+		for (int i = 0; i < nodesNum; i++) {
 			
 			if (visited[i] == false) {
 				
-				//如果是连通图，只会执行一次
 				travel(i);
 			}
 		}
@@ -105,44 +131,30 @@ public class DFSTest {
 	
 	public static void main(String[] args) {
 		
-		DFSTest dfsTest = new DFSTest(9);
-		char[] vertices = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'};
-		dfsTest.setVertices(vertices);
+		DFSTest dfsTest = new DFSTest(8);
+		
+		char[] nodes = {'1', '2', '3', '4', '5', '6', '7', '8'};
+		dfsTest.setNodes(nodes);
 		
 		dfsTest.addEdge(0, 1);
-		dfsTest.addEdge(0, 5);
+		dfsTest.addEdge(0, 2);
 		dfsTest.addEdge(1, 0);
-		dfsTest.addEdge(1, 2);
-		dfsTest.addEdge(1, 6);
-		dfsTest.addEdge(1, 8);
-		dfsTest.addEdge(2, 1);
-		dfsTest.addEdge(2, 3);
-        dfsTest.addEdge(2, 8);
-        dfsTest.addEdge(3, 2);
-        dfsTest.addEdge(3, 4);
-        dfsTest.addEdge(3, 6);
-        dfsTest.addEdge(3, 7);
-        dfsTest.addEdge(3, 8);
-        dfsTest.addEdge(4, 3);
-        dfsTest.addEdge(4, 5);
-        dfsTest.addEdge(4, 7);
-        dfsTest.addEdge(5, 0);
-        dfsTest.addEdge(5, 4);
-        dfsTest.addEdge(5, 6);
-        dfsTest.addEdge(6, 1);
-        dfsTest.addEdge(6, 3);
-        dfsTest.addEdge(6, 5);
-        dfsTest.addEdge(6, 7);
-        dfsTest.addEdge(7, 3);
-        dfsTest.addEdge(7, 4);
-        dfsTest.addEdge(7, 6);
-        dfsTest.addEdge(8, 1);
-        dfsTest.addEdge(8, 2);
-        dfsTest.addEdge(8, 3);
-        
-        System.out.println("深度遍历的递归法的处理结果是：");
-        dfsTest.DFSTravel();
-        
+		dfsTest.addEdge(1, 3);
+		dfsTest.addEdge(1, 4);
+		dfsTest.addEdge(2, 0);
+		dfsTest.addEdge(2, 5);
+		dfsTest.addEdge(2, 6);
+		dfsTest.addEdge(3, 1);
+		dfsTest.addEdge(3, 7);
+		dfsTest.addEdge(4, 1);
+		dfsTest.addEdge(4, 7);
+		dfsTest.addEdge(5, 2);
+		dfsTest.addEdge(5, 6);
+		dfsTest.addEdge(6, 2);
+		dfsTest.addEdge(6, 5);
+		dfsTest.addEdge(7, 3);
+		dfsTest.addEdge(7, 4);
+		
+		dfsTest.DFSTravel();
 	}
-	
 }
